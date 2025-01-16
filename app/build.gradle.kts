cef4f1aa1421102fd8fc95c0a84ec7e6ba2357ff
Chaima <1925046@alu.murciaeduca.es>
2025-01-16 16:18:13 +0100

    SI ME IBA, ahora estamos implementando el navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
 
+    //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -45,4 +46,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    //noinspection GradleDependency
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    //noinspection GradleDependency
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
+
 }
