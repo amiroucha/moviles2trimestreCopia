932dbb16d577f6b7bf494109f41f2a9c86f391ef
Chaima <1925046@alu.murciaeduca.es>
2025-02-04 18:33:20 +0100

    medio done nav, necesito verlo

--- app/src/main/java/com/example/loginactivitymoviles2trimestre/fragments/ContactoFragment.kt
+++ app/src/main/java/com/example/loginactivitymoviles2trimestre/fragments/ContactoFragment.kt
@@ -22,7 +22,6 @@ class ContactoFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
